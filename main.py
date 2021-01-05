4cb2904e29015fae1999ffa8a91976756a2dec05
“Benjamin <benjihuang03@gmail.com>
2021-01-05 11:36:30 -0800

    quit option implemented

--- main.py
+++ main.py
@@ -17,26 +17,46 @@ WE CAN'T LET THEM HAVE IT.
 ONE GOAL: SURVIVAL... AND THE CAR.
 REACH THE END BEFORE THE MEN GON GETCHU."""
 
-def main():
-    pass
+CHOICES = """
+    ----
+    Q. QUIT
+    ----
+"""
 
-    #TODO: Display introduction
+def intro():
     for char in textwrap.dedent(INTRODUCTION):
         time.sleep(0.05)
         sys.stdout.write(char)
         sys.stdout.flush()
 
-    time.sleep(1)
+        time.sleep(1)
 
+def main():
+    #intro()
+
+    # Variables
+    done = False
 
     #MAIN LOOP
+    while not done:
+
         #TODO: Check if reached END GAME
 
         #TODO: Present the user with their choices
+        print(CHOICES)
+
+        user_choice = input("WHAT DO YOU WANT TO DO?").lower().strip("!?,.")
+
+        if user_choice == "q":
+            done = True
 
         #TODO Change the environment based on user choice and RNG
 
         #TODO: Random event generator
+
+    # Outro
+    print("THANKS FOR PLAYING, HOPE TO SEE YOU AGAIN SOON")
+
 if __name__ == "__main__":
     main()
 
